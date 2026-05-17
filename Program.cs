06bd288f61967bab0a1d860bd646e9c350432539
Qcoder <3185566911@qq.com>
2026-05-17 20:36:04 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace C__study_5_17
+{
+    class Test
+    {
+        private int _age;
+        public int Age
+        { 
+            get{ return _age; }
+            set {  _age = value; }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Test t = new Test();
+            //t.show();
+            //int? i = null;
+            //int[] a = new int[10];
+            // int[] a = {1,2,3,4};
+            //Console.WriteLine( a.GetLength(1));
+            //int result = int.Parse(Console.ReadLine());
+            //Console.WriteLine(result);
+            
+            //foreach(var v in arr)
+            //{
+            //    Console.WriteLine(v);
+            //}
+           
+        }
+    }
+}
