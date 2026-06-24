ec37bf7d8d6927d75e4a995c3dbb641cf17c8fcf
Qcoder <3185566911@qq.com>
2026-06-24 20:43:39 +0800

    Add files via upload

---
+++ AccountModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HpGameServerDemo
+{
+    public class AccountModel
+    {
+        public int ID;
+        public string Account;
+        public string Password;
+    }
+}
