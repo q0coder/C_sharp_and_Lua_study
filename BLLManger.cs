5a4cb5a6d158c4bdc8c78549321d0d8dffbe5958
Qcoder <3185566911@qq.com>
2026-07-05 21:56:40 +0800

    Add files via upload

---
+++ BLLManger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HpGameServerDemo
+{
+    public class BLLManger
+    {
+        private static BLLManger instance;
+        public static BLLManger Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance= new BLLManger();
+                }
+                return instance;
+
+            }
+        }
+
+        //所有BLL类都在这里创建
+        public AccountBLL account = new AccountBLL();
+        public UserBLL user = new UserBLL();
+        public GameBLL game = new GameBLL();
+    }
+}
