e0ff8a1988cf0e75830f8d53bb09e24937e28c7a
Qcoder <3185566911@qq.com>
2026-07-12 22:42:39 +0800

    Add files via upload

---
+++ DALManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HpGameServerDemo
+{
+    public class DALManager
+    {
+        private static DALManager instance;
+        public static DALManager Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new DALManager();
+
+                }
+                return instance;
+
+            }
+
+        }
+        //账号数据
+        public AccountDAL account =new AccountDAL();
+        public UserDAL user = new UserDAL();
+    }
+}
